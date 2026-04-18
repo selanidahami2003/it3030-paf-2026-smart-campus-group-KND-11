ee806f8f23bed8922abdfd23c14a85f85e14ddfb
Sanduu09 <amandisanduu@gmail.com>
2026-04-18 09:32:28 +0530

    Fix merge conflict and duplicate render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-=======
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
@@ -20,4 +11,4 @@ createRoot(document.getElementById('root')).render(
     </GoogleOAuthProvider>
   </StrictMode>,
 )
->>>>>>> c327aea (Add facilities module)
+
