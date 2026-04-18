54af2f43aafb788adab9aaab9d271afc698d4b2e
Sanduu09 <amandisanduu@gmail.com>
2026-04-18 09:43:31 +0530

    Fix merge conflict in App.jsx (use facilities module)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-=======
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
->>>>>>> c327aea (Add facilities module)
 import './index.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <div className="app-container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-=======
     <AuthProvider>
       <BrowserRouter>
         <div className="app-container">
@@ -38,12 +21,7 @@ function App() {
         </div>
       </BrowserRouter>
     </AuthProvider>
->>>>>>> c327aea (Add facilities module)
   );
 }
 
 export default App;
-<<<<<<< HEAD
-
-=======
->>>>>>> c327aea (Add facilities module)
